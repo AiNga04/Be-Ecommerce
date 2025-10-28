b13a39796b0994eab5b2e8ff6377482668533114
AiNga04 <aingaspkt@gmail.com>
2025-10-28 16:23:22 +0700

    init source code

---
+++ src/main/java/com/zyna/dev/ecommerce/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.zyna.dev.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
